790ef13fcf47998ba9106170791d155a1bd18445
Mr丶Cps <1402333029@qq.com>
2019-09-04 14:51:15 +0800

    简单修改电脑访问信息

--- src/App.vue
+++ src/App.vue
@@ -63,29 +63,33 @@ export default {
 
   },
   methods:{
-    getDeviceInfo(){
-      var device_type = navigator.userAgent;//获取userAgent信息
-      var md = new MobileDetect(device_type);//初始化mobile-detect
-      var os = md.os();//获取系统
-      var model = "";
-      if (os == "iOS") {//ios系统的处理
-        os = md.os() + md.version("iPhone");
-        model = md.mobile();
-      } else if (os == "AndroidOS") {//Android系统的处理
-        os = md.os() + md.version("Android");
-        var sss = device_type.split(";");
-        var i = sss.contains("Build/");
-        if (i > -1) {
-          model = sss[i].substring(1, sss[i].indexOf("Build/"));
+      getDeviceInfo(){
+        var device_type = navigator.userAgent;//获取userAgent信息
+        var md = new MobileDetect(device_type);//初始化mobile-detect
+        var os = md.os();//获取系统
+        var model = "";
+        if (os == "iOS") {//ios系统的处理
+          os = md.os() + md.version("iPhone");
+          model = md.mobile();
+        } else if (os == "AndroidOS") {//Android系统的处理
+          os = md.os() + md.version("Android");
+          var sss = device_type.split(";");
+          var i = sss.contains("Build/");
+          if (i > -1) {
+            model = sss[i].substring(1, sss[i].indexOf("Build/"));
+          }
+        }else{
+          let kuoHaoBegin = device_type.indexOf('(');
+          let kuoHaoEnd = device_type.indexOf(')');
+          let str = device_type.substring(kuoHaoBegin + 1,kuoHaoEnd).split(';');
+          os = str[0];
+          model = '';
         }
-      }else{
-        let kuoHaoBegin = device_type.indexOf('(');
-        let kuoHaoEnd = device_type.indexOf(')');
-        let str = device_type.substring(kuoHaoBegin + 1,kuoHaoEnd).split(';');
-        os = str[0];
-        model = str[1];
-      }
-        return model + "---" + os;//手机型号和系统版本
+        if(model==''){
+          return os;  //电脑返回系统
+        }else{
+          return model + "---" + os;//手机型号和系统版本
+        }
       }
   }
  
