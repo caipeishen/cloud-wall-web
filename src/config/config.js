70dd7e488f7a86d8ab9d04da9ea1b70e4dd09092
Mr丶Cps <1402333029@qq.com>
2019-09-30 10:34:45 +0800

    修改数据源

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
-const devUrl = 'http://114.116.190.45/cloud-wall/';
-const proUrl = 'http://114.116.190.45/cloud-wall/';
+const devUrl = 'http://192.168.2.2:9090/';
+const proUrl = 'http://114.116.190.45/cloud-wall-back/';
 export default {
   apiUrl: process.env.NODE_ENV === 'production' ? proUrl : devUrl
 }
